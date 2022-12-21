cfe19b1f76a495e29bba5990efef661a8658dd47
HiMyNameIsDavidKim <rkfka1401@gmail.com>
2022-12-21 18:01:44 +0900

    12/21 study update

--- users/services.py
+++ users/services.py
@@ -1,5 +1,31 @@
+import random
+import string
+
+
 class UserService(object):
     def __init__(self):
-        pass
+        global id, username, password
+        id = []
+        username = []
+        password = []
 
-    
+    def dummy_df(self):
+        id_set = string.ascii_letters + string.digits
+        name_set = string.ascii_letters
+
+        while len(id) != 100:
+            rand_id = ''.join(random.sample(id_set, 10))
+            rand_name = ''.join(random.sample(name_set, 5))
+            if (rand_id in id) or (rand_name in username):
+                pass
+            else:
+                id.append(rand_id)
+                username.append(rand_name)
+                password.append(str(123))
+        df = [id, username, password]
+        return df
+
+if __name__ == '__main__':
+    us = UserService()
+    df = us.dummy_df()
+    print(df)
